becbaba41e25dc15456e108b8cb894a73dc69d0b
bishwajit01 <bishwajit.vikram@gmail.com>
2020-01-17 00:59:34 +0530

    Update the git ignore with one To Many and Added project for Hibernate Mapping the One to Many.

---
+++ OneToManyMapping/src/com/vikram/bishwajit/dto/UserDetails.java
@@ -0,0 +1,76 @@
+package com.vikram.bishwajit.dto;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ * @author Bishwajit.
+ *
+ */
+@Entity
+@Table(name = "USER_DETAILS")
+public class UserDetails {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "USER_ID")
+	private int userId;
+
+	@Column(name = "USER_NAME")
+	private String userName;
+
+	@OneToMany
+	@Column(name = "VEHILCE_ID_1_N")
+	private Collection<Vehicle> listOfVehicle = new ArrayList<Vehicle>();
+
+	/**
+	 * @return the userId
+	 */
+	public int getUserId() {
+		return userId;
+	}
+
+	/**
+	 * @param userId the userId to set
+	 */
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+
+	/**
+	 * @param userName the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	/**
+	 * @return the listOfVehicle
+	 */
+	public Collection<Vehicle> getListOfVehicle() {
+		return listOfVehicle;
+	}
+
+	/**
+	 * @param listOfVehicle the listOfVehicle to set
+	 */
+	public void setListOfVehicle(Collection<Vehicle> listOfVehicle) {
+		this.listOfVehicle = listOfVehicle;
+	}
+
+}
