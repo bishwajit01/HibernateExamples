abfdedcc717645e5f2c1ef9079d037fc7df981f3
bishwajit01 <bishwajit.vikram@gmail.com>
2020-01-17 22:49:31 +0530

    Many to Many mapping in hibernates.

---
+++ ManyToManyMapping/src/com/vikram/bishwajit/dto/Vehicle.java
@@ -0,0 +1,78 @@
+/**
+ * 
+ */
+package com.vikram.bishwajit.dto;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+/**
+ * @author Bishwajit.
+ *
+ */
+@Entity
+@Table(name = "VEHICLE")
+public class Vehicle {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "VEHICLE_ID")
+	private int vehicleId;
+
+	@Column(name = "VEHICLE_NAME")
+	private String vehicleName;
+
+	@ManyToMany
+	private Collection<UserDetails> userDetails = new ArrayList<UserDetails>();
+
+	/**
+	 * @return the vehicleId
+	 */
+	public int getVehicleId() {
+		return vehicleId;
+	}
+
+	/**
+	 * @param vehicleId the vehicleId to set
+	 */
+	public void setVehicleId(int vehicleId) {
+		this.vehicleId = vehicleId;
+	}
+
+	/**
+	 * @return the vehicleName
+	 */
+	public String getVehicleName() {
+		return vehicleName;
+	}
+
+	/**
+	 * @param vehicleName the vehicleName to set
+	 */
+	public void setVehicleName(String vehicleName) {
+		this.vehicleName = vehicleName;
+	}
+
+	/**
+	 * @return the userDetails
+	 */
+	public Collection<UserDetails> getUserDetails() {
+		return userDetails;
+	}
+
+	/**
+	 * @param userDetails the userDetails to set
+	 */
+	public void setUserDetails(Collection<UserDetails> userDetails) {
+		this.userDetails = userDetails;
+	}
+
+}
